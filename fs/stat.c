b234ef9f94db8eeb0367116fd993d7a38fea3a6c
Vaiishnavi <ns.nznz77777.com>
2026-09-12 02:05:01 +0700

    fs/stat: Fix statx_sus_kstat

--- fs/stat.c
+++ fs/stat.c
@@ -19,10 +19,8 @@
 #include <asm/unistd.h>
 
 #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-extern bool susfs_is_inode_sus_kstat(struct inode *inode, bool *out_is_fuse);
-extern void susfs_sus_kstat_spoof_generic_fillattr(struct inode *inode, struct kstat *stat, u32 result_mask);
-#endif // #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-
+extern void susfs_sus_kstat_spoof_generic_fillattr(struct inode *inode, struct kstat *stat);
+#endif
 void generic_fillattr(struct inode *inode, struct kstat *stat)
 {
 	stat->dev = inode->i_sb->s_dev;
@@ -38,6 +36,9 @@ void generic_fillattr(struct inode *inode, struct kstat *stat)
 	stat->ctime = inode->i_ctime;
 	stat->blksize = i_blocksize(inode);
 	stat->blocks = inode->i_blocks;
+#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
+	susfs_sus_kstat_spoof_generic_fillattr(inode, stat);
+#endif
 }
 
 EXPORT_SYMBOL(generic_fillattr);
@@ -58,56 +59,17 @@ int vfs_getattr_nosec(struct path *path, struct kstat *stat)
 {
 	struct inode *inode = d_backing_inode(path->dentry);
 
-#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-	if (susfs_is_current_app_uid()) {
-		bool is_fuse = false;
-		if (susfs_is_inode_sus_kstat(d_backing_inode(path->dentry), &is_fuse)) {
-			if (!is_fuse) {
-				// stat->mnt_id = real_mount(path->mnt)->mnt_id;
-				// only for 5.10 kernel
-				stat->result_mask |= STATX_SUS_KSTAT;
-			}
-			// stat->mnt_id = real_mount(path->mnt)->mnt_id;
-			// only for 5.10 kernel
-			stat->result_mask |= STATX_SUS_KSTAT_FUSE;
-		}
-	}
-#endif // #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-
-#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-	if (inode->i_op->getattr) {
-#else
 	if (inode->i_op->getattr)
-#endif
-
 #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
+	{
 		int err = inode->i_op->getattr(path->mnt, path->dentry, stat);
-		if (!err) {
-			if (stat->result_mask & STATX_SUS_KSTAT) {
-				susfs_sus_kstat_spoof_generic_fillattr(inode, stat, STATX_SUS_KSTAT);
-				return err;
-			}
-			if (stat->result_mask & STATX_SUS_KSTAT_FUSE) {
-				susfs_sus_kstat_spoof_generic_fillattr(inode, stat, STATX_SUS_KSTAT_FUSE);
-				return err;
-			}
-		}
+		if (!err)
+			susfs_sus_kstat_spoof_generic_fillattr(inode, stat);
 		return err;
-	}
-	if (stat->result_mask & STATX_SUS_KSTAT) {
-		generic_fillattr(inode, stat);
-		susfs_sus_kstat_spoof_generic_fillattr(inode, stat, STATX_SUS_KSTAT);
-		return 0;
-	}
-	if (stat->result_mask & STATX_SUS_KSTAT_FUSE) {
-		generic_fillattr(inode, stat);
-		susfs_sus_kstat_spoof_generic_fillattr(inode, stat, STATX_SUS_KSTAT_FUSE);
-		return 0;
 	}
 #else
 		return inode->i_op->getattr(path->mnt, path->dentry, stat);
-#endif // #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-
+#endif
 	generic_fillattr(inode, stat);
 	return 0;
 }
